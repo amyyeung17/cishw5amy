fc7399b5078a4aaf86f42379c276f15384e818ef
Amy Yeung <amyyeung@seas.upenn.edu>
2021-05-04 16:24:53 -0400

    files

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,135 @@
-import React from 'react'
-
-import SaveIntro from './SaveIntro'
-import EditIntro from './EditIntro'
-import AddPost from './AddPost'
+import React, { useState, useEffect } from 'react'
+import s from 'styled-components'
 import Post from './Post'
 
-const App = () => (
-  <>
-    <h1 style={{ fontFamily: 'monospace', fontSize: '38px' }}>Hey this is me!</h1>
-    <SaveIntro />
-    <EditIntro />
-    <h1 style={{
-      fontFamily: 'monospace', fontSize: '38px', position: 'relative', marginBottom: '25px',
-    }}
-    >
-      Blog Posts
-    </h1>
-    <AddPost />
-    <Post />
-  </>
-)
+const Styledbutton = s.button`
+  width: 17.5%;
+  height: 15%;
+  font-size: 16px;
+  border-width: 0px;
+  border-radius: 5px;
+  right: 25px;
+  background-color: rgb(13, 95, 248);
+  color:white;
+  font-family:Avenir;
+  font-size:20px;
+  font-weight: 450;
+`
+
+const Styledbutton1 = s.button`
+  width: 7%;
+  height: 15%;
+  font-size: 16px;
+  border-width: 0px;
+  border-radius: 5px;
+  right: 25px;
+  background-color: rgb(13, 95, 248);
+  color:white;
+  font-family:Avenir;
+  font-size:20px;
+  font-weight: 450;
+`
+
+const StyleBox = s.div`
+  display:flex; 
+  justify-content: center; 
+  z-index: -10;
+`
+const Po = s.div`
+  width: 730px; 
+  height: 350px;
+  border: 2px solid lightgrey;
+  border-radius: 5px;
+`
+
+const Input = s.textarea`
+  height: 125px;
+  width: 670px;
+  border: 2px solid lightgrey;
+  margin-left: 25px;
+  margin-right: 25px;
+  margin-top: 15px;
+  font-family: Avenir;
+  font-size: 22px;
+  border-radius: 5px;
+`
+const Title = s.input`
+  height: 40px; 
+  width: 670px;
+  fontSize: 28px; 
+  border: 2px solid lightgrey;
+  margin-left: 25px;
+  margin-right: 25px;
+  margin-top: 15px;
+  font-family: Avenir;
+  font-size: 28px;
+  border-radius: 5px;
+`
+
+const Arrow =s.button`
+  border: solid salmon;
+  border-width: 0 7px 7px 0;
+  display: inline-block;
+  padding: 5px;
+  transform: rotate(45deg);
+  -webkit-transform: rotate(45deg);
+  z-index: 3;
+`
+
+const recursive = ({p}) => {
+  return(
+    <>
+    {p !== undefined && p.length !== 0 && p.map(p => {
+      return(
+      <Post p={p}/>
+      )
+    })}
+    </>
+  )
+}
+
+const App = () => {
+  const [name, setName] = useState('')
+  const [text, setText] = useState('')
+  const [likes, setLikes] = useState(0)
+  const [dislikes, setDislikes] = useState(0)
+  const [state, setState] = useState('')
+  const [p, setP] = useState([])
+  const [input, setInput] = useState(false)
+  const [c, setC] = useState([])
+  const [reply, setReply] = useState('')
+ 
+
+
+
+  return (
+    <>
+      <h1 style={{
+        fontFamily: 'Avenir-Black', fontSize: '38px', marginTop: '40px', justifyContent: 'center', display: 'flex',
+      }}
+      >
+        🎉 My Community🎉
+      </h1>
+      <StyleBox>
+        <Po>
+          <h3 style={{
+            fontFamily: 'Avenir-Black', fontSize: '32px', marginLeft: '25px', marginBottom: '0px',
+          }}
+          >
+            New Post
+          </h3>
+          <Title placeholder="Name...." onChange={e => setName(e.target.value)} />
+          <Input placeholder="Start typing here....." onChange={e => setText(e.target.value)} />
+          <Styledbutton
+            style={{ position: 'relative', left: '80%', marginTop: '2.5px' }}
+            onClick={() => (name !== '' && text !== '' ? (p.push([name, text, likes, dislikes, [], false]), setP(p), setC(!c)) : '')}
+          >
+            Submit
+          </Styledbutton>
+        </Po>
+      </StyleBox>
+    {recursive({p})}
+    </>
+  )
+}
 export default App
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,162 +1,158 @@
-import React, { useState } from 'react'
-import { connect } from 'react-redux'
+import React, { useState, useEffect } from 'react'
 import s from 'styled-components'
-import Popup from 'reactjs-popup'
 
-import { editPost, deletePost } from '../actions'
 
-const Styledpost = s.div`
-  width: 350px;
-  height: 350px;
-  border-width: 3px;
-  border-style: solid;
-  border-color: #B6CFCA;
-  border-radius: 3px;
-  display: flex;
-  flex-direction: column;
-  flex-basis: auto;
-  align-items: center
-`
 const Styledbutton = s.button`
-  background-color: #F2ECAD;
-  width: 25%;
-  height: 10%;
-  font-size: 14px;
-  font-family: monospace;
-  border-width: 2px;
+  width: 17.5%;
+  height: 15%;
+  font-size: 16px;
+  border-width: 0px;
   border-radius: 5px;
-  border-color: #F3F3F3
+  right: 25px;
+  background-color: rgb(13, 95, 248);
+  color:white;
+  font-family:Avenir;
+  font-size:20px;
+  font-weight: 450;
+`
+
+const StyleBox = s.div`
+  display:flex; 
+  justify-content: center; 
+  z-index: -10;
+`
+const P = s.div`
+  width: 730px; 
+  height: 100px;
+  border: 2px solid lightgrey;
+  border-radius: 5px;
+`
+
+const Input = s.textarea`
+  height: 125px;
+  width: 670px;
+  border: 2px solid lightgrey;
+  margin-left: 25px;
+  margin-right: 25px;
+  margin-top: 15px;
+  font-family: Avenir;
+  font-size: 22px;
+  border-radius: 5px;
+`
+const Title = s.input`
+  height: 40px; 
+  width: 670px;
+  fontSize: 28px; 
+  border: 2px solid lightgrey;
+  margin-left: 25px;
+  margin-right: 25px;
+  margin-top: 15px;
+  font-family: Avenir;
+  font-size: 28px;
+  border-radius: 5px;
+`
+const Po = s.div`
+  width: 730px; 
+  height: 350px;
+  border: 2px solid lightgrey;
+  border-radius: 5px;
 `
 
 const Styledbutton1 = s.button`
-  width: 20%;
-  height: 10%;
+  width: 7%;
+  height: 15%;
   font-size: 16px;
-  font-family: monospace;
-  border-width: 1px;
+  border-width: 0px;
   border-radius: 5px;
-  position:relative;
-  border-color: #F3F3F3
-`
-const Styledmenu = s.div`
-  width: 400px;
-  height: 400px;
-  border-width: 2px;
-  border-style: solid;
-  border-color: #B6CFB6;
-  top: 75px;
-  border-radius: 3px;
-  align-items: center;
-  position: relative;
-  background-color: white
+  right: 25px;
+  background-color: rgb(13, 95, 248);
+  color:white;
+  font-family:Avenir;
+  font-size:20px;
+  font-weight: 450;
 `
 
-const Styleditem = s.input`
-  width: 350px;
-  height: 35px;
-  border-radius: 3px;
-  border-width: 1.5px;
-  border-style: solid;
-  border-color: #FAE9E4;
-  align-items: center;
-  left: 25;
-  position: absolute
-`
 
-const Styleddesc = s.h2`
-  font-family: monospace;
-  position: absolute
-`
+const Post = ({ p }) => {
+  const [name, setName] = useState('')
+  const [text, setText] = useState('')
+  const [likes, setLikes] = useState(0)
+  const [dislikes, setDislikes] = useState(0)
+  const [state, setState] = useState('')
+  const [posts, setPost] = useState(p)
+  const [input, setInput] = useState(false)
+  const [b, setB] = useState(false)
+  const [n, setN] = useState('')
+  const [reply, setReply] = useState('')
+  const counter = 0
 
-const Container = s.div`
-  display: inline-flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  margin: 25px
-`
 
-const Helper = ({ dispatchEditPost, dispatchDeletePost, p}) => {
-  const [newTitle, setNewTitle] = useState(p.title)
-  const [newImage, setNewImage] = useState(p.image)
-  const [newDesc, setNewDesc] = useState(p.text)
-  const [popUp, setNewPop] = useState(false)
+  const id = "id" + Math.random().toString(16).slice(2)
+
+  useEffect(async () => {
+    setPost(posts)
+  }, [posts, b])
+  
+  useEffect(() => {
+    const intervalID = setInterval(() => {}, 50)
+    return ()=>clearInterval(intervalID)
+  }, [])
+
+  if(posts.length === 0) {
+
+    return null
+  } 
+
+
 
   return (
     <>
-      <Container>
-        <Styledpost>
-          <h1 style={{fontFamily: 'monospace'}}> {p.title}</h1>
-          <img style={{maxWidth: '250px', maxHeight: '250px'}} src = {p.image}/>
-          <p style={{fontFamily:'monospace'}}> {p.text} </p>
-          <Styledbutton
-            onClick={() => {
-              setNewPop(true)
-            }}
-          >
-            Edit Post
-          </Styledbutton>
-        </Styledpost>
-      </Container>
-      <Popup open={popUp} closeOnDocumentClick onClose={!popUp} modal nested>
-        <Styledmenu>
-          <ul>
-            <Styleddesc style={{ top: '40px' }}>Title</Styleddesc>
-            <Styleditem style={{ top: '85px' }} value={newTitle} onChange={e => setNewTitle(e.target.value)} />
-            <Styleddesc style={{ top: '120px' }}>Image</Styleddesc>
-            <Styleditem style={{ top: '165px' }} value={newImage} onChange={f => setNewImage(f.target.value)} />
-            <Styleddesc style={{ top: '200px' }}>Desc</Styleddesc>
-            <Styleditem style={{ top: '245px' }} value={newDesc} onChange={g => setNewDesc(g.target.value)} />
-          </ul>
-          <Styledbutton1
-            style={{ backgroundColor: '#B2F2AD', left: '20px', top: '310px' }}
-            onClick={() => {
-              dispatchEditPost(newTitle, newImage, newDesc, p.id)
-              setNewPop(false)
-            }}
-          >
-            Save
-          </Styledbutton1>
-          <Styledbutton1
-            style={{ backgroundColor: '#ADF2E4', left: '35px', top: '310px' }}
-            onClick={() => setNewPop(false)}>
-            Cancel
-          </Styledbutton1>
-          <Styledbutton1
-            style={{ backgroundColor: '#F39D88', left: '50px', top: '310px' }}
-            onClick={() => {
-              dispatchDeletePost(p.id)
-              setNewPop(false)
-            }}
-          >
-            Delete
-          </Styledbutton1>
-        </Styledmenu>
-
-      </Popup>
+       <div style={{display: 'flex', flexDirection:'column', alignItems:'center'}}>
+            <>
+              <Po key={id.uniqueId} style={{height:'200px', marginTop:'10px'}}>
+                <h2 style={{ fontFamily: 'Avenir', fontSize: '32px'}}>{posts[0]}</h2>
+                <p style={{ fontFamily: 'Avenir', fontSize: '22px'}}>{posts[1]}</p>
+              <Styledbutton
+                style={{ backgroundColor: 'gray' }}
+                onClick={() => {posts[5] = !posts[5]; setInput(!input)}}
+              >
+                reply
+              </Styledbutton>
+              <Styledbutton1 key={id.uniqueId} style={{position: 'relative', left:'499.5px', top: '-25px'}} 
+                onClick={() => posts[3] -= 1}>  
+                v
+              </Styledbutton1>
+              <Styledbutton1 key={id.uniqueId} style={{position: 'relative', left:'448.5px', top: '-120px'}}
+                onClick={() => {posts[3] += 1}}> 
+                ^
+              </Styledbutton1>
+              <p style={{ fontFamily: 'Avenir', fontSize: '22px', position:'relative', left: '646px', top:'-125px'}}>{posts[3]}</p>
+            </Po>  
+            </>
+            {(posts[5] && posts[4].length < 3)?
+              ( 
+                <>
+                  <Po key={p[0]} style={{ backgroundColor: 'lightgray', height:'190px' }}>
+                    <Title placeholder="Name..." onChange={e => setN(e.target.value)} style={{height:'30px' }}/>
+                    <Input placeholder="Write your reply here" onChange={e => setReply(e.target.value)} style={{height:'75px'}}/>
+                    <Styledbutton
+                      style={{ backgroundColor: 'gray' }}
+                      onClick={() => (n !== '' && reply !== '' ? (posts[4].push([n, reply, 0, 0, [], false]), setInput(!input), setPost(posts[4]), posts[5] = !posts[5]) : '')}
+                    >
+                      comment
+                    </Styledbutton>
+                  </Po>
+                </>
+              )
+              
+              
+              : null}
+              
+            
+          </div>
     </>
+    
   )
 }
 
-const Post = ({ dispatchEditPost, dispatchDeletePost, post }) => (
-  <>
-    {post.map(p => (
-      <Helper
-        dispatchEditPost={dispatchEditPost}
-        dispatchDeletePost={dispatchDeletePost}
-        p={p}
-        key={p.id}
-      />
-    ))}
-  </>
-)
-
-const mapDispatchToProps = dispatch => ({
-  dispatchEditPost: (title, image, text, id) => dispatch(editPost(title, image, text, id)),
-  dispatchDeletePost: id => dispatch(deletePost(id)),
-})
-
-const mapStateToProps = state => ({
-  post: state.post,
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(Post)
+export default Post
